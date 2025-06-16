ec3ad3a53c760091b0ef16428dae46bac8772b61
UrokiVV <vvsoft@yahoo.com>
2025-06-16 19:48:44 +0300

    N5 func master

--- main_First  jun16.py
+++ main_First  jun16.py
@@ -10,3 +10,15 @@ print("Чтобы хорошо программировать -  надо мно
 print(" A1) Вот и всё. \n  я звоню вам с вокзала.")
 print("  Я спешу. \n  Извините меня!")
 
+def func(x1):
+    pi = 3.14
+    p = pi *2 *x1
+    s = pi * x1 *x1
+    res = {"p": p, "s": s}
+    return res
+x = 5
+res = func(x)
+print(f"x={x} : func({x}) ==> p={res['p']:.3}, s={res['s']:.4}")
+print("OK master")
+
+
