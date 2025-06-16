90c3c3ce6c9adda7c720447c620912d78c9b9846
UrokiVV <vvsoft@yahoo.com>
2025-06-16 17:39:49 +0300

    N7 Dev2 func

--- main_First  jun16.py
+++ main_First  jun16.py
@@ -1,6 +1,15 @@
 # main_First  jun16
 
 print(" A1) Privet ! \n main_First")
-print("Мы пишем, пишем, пишеи... ")
+print("Dev2) \nМы пишем, пишем, пишеи... ")
 print("В далёкие края!")
 
+def func(x1):
+    p = 4 *x1
+    s = x1 *x1
+    res = {"p": p, "s": s}
+    return res
+x = 5.0
+res = func(x)
+print(f"\n x={x} : func({x}) ==> p={res['p']:.3}, s={res['s']:.4}")
+print("OK Dev2")
