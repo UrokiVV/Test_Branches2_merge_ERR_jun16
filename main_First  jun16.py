f3b7889a493e6605c4194640be5353e108bc66ee
UrokiVV <vvsoft@yahoo.com>
2025-06-16 21:37:41 +0300

    N9 Dev2

--- main_First  jun16.py
+++ main_First  jun16.py
@@ -5,10 +5,11 @@ print("Dev2) \nМы пишем, пишем, пишеи... ")
 print("В далёкие края!")
 
 def func(x1):
-    p = 4 *x1
-    s = x1 *x1
+    p = 4 * x1
+    s = x1 * x1
     res = {"p": p, "s": s}
     return res
+
 x = 5.0
 res = func(x)
 print(f"\n x={x} : func({x}) ==> p={res['p']:.3}, s={res['s']:.4}")
