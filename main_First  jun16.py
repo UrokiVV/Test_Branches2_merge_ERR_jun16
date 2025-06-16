277474622d57194e5824803533cdba8505dc5d89
UrokiVV <vvsoft@yahoo.com>
2025-06-16 21:54:37 +0300

    N10 Dev2 funct2

--- main_First  jun16.py
+++ main_First  jun16.py
@@ -4,13 +4,15 @@ print(" A1) Privet ! \n main_First")
 print("Dev2) \nМы пишем, пишем, пишеи... ")
 print("В далёкие края!")
 
-def func(x1):
+
+def func2(x1):
     p = 4 * x1
     s = x1 * x1
-    res = {"p": p, "s": s}
-    return res
+    result = {"p": p, "s": s}
+    return result
+
 
 x = 5.0
-res = func(x)
-print(f"\n x={x} : func({x}) ==> p={res['p']:.3}, s={res['s']:.4}")
+res = func2(x)
+print(f"\n x={x} : func2({x}) ==> p={res['p']:.3}, s={res['s']:.4}")
 print("OK Dev2")
